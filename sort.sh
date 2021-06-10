bc76591f17a0be45bb5a80492978abfb22ca43de
originalcybernaut <59384941+originalcybernaut@users.noreply.github.com>
2021-06-09 23:15:29 -0400

    Update sort.sh

--- sort.sh
+++ sort.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
-cat $1 | sort -t '.' -n
+#!/usr/bin/bash  
+cat $1 | sort -t '.' -n  
 
-# one liner to sort IP addresses numerically in ascending order
+# one liner to sort IP addresses numerically in ascending order  
+# input text file with IP addresses as first argument  
